a9b7ec064f1b001d622792451d9d9ae4c143807b
FERNANDO_CABRERA <fernandocabrerabrz@gmail.com>
2025-06-05 11:01:41 +0000

    imagen generada

--- agent_individual/create_images/generate_image.py
+++ agent_individual/create_images/generate_image.py
@@ -17,21 +17,28 @@ api_token = HUGGING['api']
 import os
 
 client = InferenceClient(
-    provider="nscale",
     api_key=api_token,
 )
-
+#prompt
+prompt = "Un empaque de snacks saludables, diseño moderno y minimalista, colores verde y blanco, fondo blanco con luz natural, frutas frescas, estilo fotográfico profesional"
 # output is a PIL.Image object
 image = client.text_to_image(
-    "Astronaut riding a horse",
+    prompt,
     model="black-forest-labs/FLUX.1-dev",
 )
 
-images_dir = "images"
+# Obtiene la ruta absoluta del directorio donde está este script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Define la ruta de la carpeta images al mismo nivel que el script
+images_dir = os.path.join(script_dir, "images")
 os.makedirs(images_dir, exist_ok=True)
-# Nombre del archivo (ej: imagen_20250604_1452.png)
+
+# Define el nombre del archivo
 filename = f"imagen_{datetime.now().strftime('%Y%m%d_%H%M%S')}.png"
 image_path = os.path.join(images_dir, filename)
+
+# Guarda la imagen
 image.save(image_path)
 print(f"✅ Imagen guardada en: {image_path}")
 
