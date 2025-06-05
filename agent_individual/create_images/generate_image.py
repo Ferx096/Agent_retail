ee6131b5505480ef6069189fcc3604ac84bf3a5d
FERNANDO_CABRERA <fernandocabrerabrz@gmail.com>
2025-06-05 08:35:14 +0000

    ajustes

--- agent_individual/create_images/generate_image.py
+++ agent_individual/create_images/generate_image.py
@@ -4,7 +4,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + '/../'))
 import requests
 from config import HUGGING
-git addlogging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 api_token = HUGGING['api']
